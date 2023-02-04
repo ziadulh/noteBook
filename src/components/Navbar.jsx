f8c7f4a1417bbdf8cd277c472f696965a41ef471
ziadul <mdzhhridoy@gmail.com>
2023-02-04 20:26:23 +0600

    changeed filetype from js to jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link, useLocation } from 'react-router-dom'
 function Navbar() {
 
     let location = useLocation();
-    useEffect(() => {
-        console.log(location.pathname)
-    }, [location]);
+    // useEffect(() => {
+    //     console.log(location.pathname)
+    // }, [location]);
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
